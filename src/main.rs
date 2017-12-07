ee889c4be502d6a8881bbdf0322277b457384bee
Chen Weiguang <chen.weiguang@gmail.com>
2017-12-07 21:10:17 +0800

    [WIP] Added implementation for getting individual CPU loads, but does not work for Linux.

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,23 @@
 #[macro_use]
 extern crate failure;
 extern crate fruently;
-extern crate self_meter;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
+extern crate systemstat;
 
 use failure::Error;
 use fruently::fluent::Fluent;
 use fruently::forwardable::JsonForwardable;
-use self_meter::Meter;
 use std::thread;
+use std::collections::HashMap;
 use std::time::Duration;
 use structopt::StructOpt;
+use systemstat::{CPULoad, Platform};
+use systemstat::platform::PlatformImpl;
 
 #[derive(Debug, Fail)]
 enum FluentError {
@@ -47,11 +52,41 @@ struct MainConfig {
     interval: u64,
 }
 
-fn run_impl(addr: &str, tag: &str) -> Result<()> {
+#[derive(Serialize, Deserialize, Debug)]
+struct SerCpuLoad {
+    user: f32,
+    nice: f32,
+    system: f32,
+    interrupt: f32,
+    idle: f32,
+}
+
+impl SerCpuLoad {
+    fn from_cpu_load(c: &CPULoad) -> SerCpuLoad {
+        SerCpuLoad {
+            user: c.user,
+            nice: c.nice,
+            system: c.system,
+            interrupt: c.interrupt,
+            idle: c.idle,
+        }
+    }
+}
+
+fn run_impl(platform: &PlatformImpl, addr: &str, tag: &str) -> Result<()> {
     let fluent = Fluent::new(addr, tag);
+    let cpu_loads = &platform.cpu_load()?.done()?;
+
+    let ser_cpu_loads: HashMap<usize, SerCpuLoad> = cpu_loads
+        .into_iter()
+        .enumerate()
+        .map(|(i, cpu_load)| (i, SerCpuLoad::from_cpu_load(cpu_load)))
+        .collect();
+
+    println!("{:?}", ser_cpu_loads);
 
     fluent
-        .post("")
+        .post(&ser_cpu_loads)
         .map_err(|e| -> FluentError { e.into() })?;
 
     Ok(())
@@ -60,12 +95,10 @@ fn run_impl(addr: &str, tag: &str) -> Result<()> {
 fn run() -> Result<()> {
     let config = MainConfig::from_args();
     let interval = Duration::from_secs(config.interval);
-
-    let scan_interval: Duration = Duration::from_secs(1);
-    let mut meter = Meter::new(scan_interval)?;
+    let platform = PlatformImpl::new();
 
     loop {
-        if let Err(e) = run_impl(&config.addr, &config.tag) {
+        if let Err(e) = run_impl(&platform, &config.addr, &config.tag) {
             eprintln!("resup run ERROR: {}", e);
         }
 
