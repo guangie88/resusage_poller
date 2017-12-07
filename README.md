a6426ef5af577ef4b9f6c629be2bebfe693c8899
Chen Weiguang <chen.weiguang@gmail.com>
2017-12-07 17:11:57 +0800

    Working systemstat solution by using the latest git revision.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # resusage_poller
-Resource usage poller for Fluentd logging.
+
+`Rust` executable to poll for CPU usage (per core) based on parsing of
+`/proc/stat` and logs to `Fluentd` unified logging layer.
+
+`resup` is the executable short form for Resource Usage Poller.
+
+Requires Linux and supports compilation on stable `rustc`, and also works for
+target `x86_64-unknown-linux-musl`.
+
+## Installation of `Rust` environment
+
+Follow the instructions from [`rustup`](https://www.rustup.rs/). In order to
+build for target `x86_64-unknown-linux-musl`, the following command should be
+run **after** the installation of the toolchain:
+
+```bash
+rustup target add x86_64-unknown-linux-musl
+```
+
+The benefit building for target `x86_64-unknown-linux-musl` is that the compiled
+executable is fully statically linked, _i.e_. does not depend on any system
+shared objects and is very portable across all Linux distributions.
+
+## Recommended Build Command
+
+```bash
+cargo build --release --target x86_64-unknown-linux-musl
+```
+
+## Run Example
+
+Polls for:
+
+* (Default `Fluentd` server at: `127.0.0.1:24224`)
+* Interval: 1 second
+* `Fluentd` tag: `elastic.rs`
+
+```bash
+./target/x86_64-unknown-linux-musl/release/resup -i 1 -t elastic.rs
+```
+
+Note the program loops forever until `CTRL-C` is pressed.
---
+++ src/main.rs
@@ -0,0 +1,117 @@
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+#![cfg_attr(feature = "clippy", deny(warnings))]
+
+#[macro_use]
+extern crate failure;
+extern crate fruently;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+extern crate systemstat;
+
+use failure::Error;
+use fruently::fluent::Fluent;
+use fruently::forwardable::JsonForwardable;
+use std::thread;
+use std::collections::HashMap;
+use std::time::Duration;
+use structopt::StructOpt;
+use systemstat::{CPULoad, Platform};
+use systemstat::platform::PlatformImpl;
+
+#[derive(Debug, Fail)]
+enum FluentError {
+    #[fail(display = "")] InnerFluentError { e: fruently::error::FluentError },
+}
+
+impl From<fruently::error::FluentError> for FluentError {
+    fn from(e: fruently::error::FluentError) -> FluentError {
+        FluentError::InnerFluentError { e: e }
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "resup", about = "Resources Usage Poller")]
+struct MainConfig {
+    #[structopt(short = "a", long = "addr",
+                default_value = "127.0.0.1:24224", help = "Fruentd hostname")]
+    addr: String,
+
+    #[structopt(short = "t", long = "tag",
+                help = "Tag to use for Fruentd logging")]
+    tag: String,
+
+    #[structopt(parse(try_from_str), short = "i", long = "interval",
+                help = "Interval in seconds")]
+    interval: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SerCpuLoad {
+    user: f32,
+    nice: f32,
+    system: f32,
+    interrupt: f32,
+    idle: f32,
+}
+
+impl SerCpuLoad {
+    fn from_cpu_load(c: &CPULoad) -> SerCpuLoad {
+        SerCpuLoad {
+            user: c.user,
+            nice: c.nice,
+            system: c.system,
+            interrupt: c.interrupt,
+            idle: c.idle,
+        }
+    }
+}
+
+fn run_impl(
+    pf: &PlatformImpl,
+    addr: &str,
+    tag: &str,
+    interval: Duration,
+) -> Result<()> {
+    let cpu_loads = pf.cpu_load()?;
+
+    // required to sleep before .done() is invoked
+    thread::sleep(interval);
+    let cpu_loads = cpu_loads.done()?;
+
+    let ser_cpu_loads: HashMap<usize, SerCpuLoad> = cpu_loads
+        .into_iter()
+        .enumerate()
+        .map(|(i, cpu_load)| (i, SerCpuLoad::from_cpu_load(&cpu_load)))
+        .collect();
+
+    Fluent::new(addr, tag)
+        .post(&ser_cpu_loads)
+        .map_err(|e| -> FluentError { e.into() })?;
+
+    Ok(())
+}
+
+fn run() -> Result<()> {
+    let config = MainConfig::from_args();
+    let interval = Duration::from_secs(config.interval);
+    let pf = PlatformImpl::new();
+
+    loop {
+        if let Err(e) = run_impl(&pf, &config.addr, &config.tag, interval) {
+            eprintln!("resup run ERROR: {}", e);
+        }
+    }
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("resup main ERROR: {}", e);
+    }
+}
